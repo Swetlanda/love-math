c3d8be8c61e3ddf4917a9f24b9bca840170702c1
Lola Crispin <swetlanda@gmail.com>
2024-06-20 14:41:28 +0000

    JS - add evlick Sumbit and Gametype

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+// added event listener, use TagName for all buttons
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    // return of getElementByTagName is Array so we are using a loop for all eleements inside (instead of let i = 0;etc)
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+
+            // getAttribute get the value of clicked button to check if this is Submit button using data-type - Submit
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+        }
+    });
+}
+});
+
 function runGame() {
 
 }
