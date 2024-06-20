085eb3eb879c3ddb05b639ca6917fc0e58a07282
Lola Crispin <swetlanda@gmail.com>
2024-06-20 14:48:12 +0000

    JS - added runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,34 @@
 // Get the button elements and add event listeners to them
 
 // added event listener, use TagName for all buttons
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     // return of getElementByTagName is Array so we are using a loop for all eleements inside (instead of let i = 0;etc)
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
 
-            // getAttribute get the value of clicked button to check if this is Submit button using data-type - Submit
+            // getAttribute get the value of clicked button to check if this is Submit button using data-type - Submit, and other data-type for each of the math operations
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
-        }
-    });
-}
+            }
+        });
+    }
 });
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 
 function runGame() {
 
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
@@ -36,6 +44,7 @@ function incrementScore() {
 
 }
 
+
 function incrementWrongAnswer() {
 
 }
@@ -49,5 +58,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
